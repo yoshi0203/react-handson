67fd2ff44e466f3726bb14002118210b0c044f29
Yoshito Yamanaka <ymnkswallow@gmail.com>
2026-02-10 18:32:07 +0900

    first commit

---
+++ react-guide-material/03_js_basic/020_arrow_function/start/main.js
@@ -0,0 +1,7 @@
+function fn(number) {
+  return number * 2;
+}
+
+const fnArrow = number => number * 2;
+
+console.log(fnArrow(1));
---
+++ react-guide-material/03_js_basic/030_esmodule/start/main.js
@@ -0,0 +1,10 @@
+import { hello } from "./module.js";
+import { User } from "./module.js";
+import fanctionB from "./module.js";
+
+hello();
+
+const user = new User('Tom');
+user.hello();
+
+fanctionB();
