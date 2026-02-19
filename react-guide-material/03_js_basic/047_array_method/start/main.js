e307675edf51ef6ec1ba32b19d6928de56a1f54d
Yoshito Yamanaka <ymnkswallow@gmail.com>
2026-02-20 01:46:28 +0900

    fix-16_mapメソッドfilterメソッド

--- react-guide-material/03_js_basic/047_array_method/start/main.js
+++ react-guide-material/03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,13 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
+
+for (let i = 0; i < arry.length; i++) {
+  newArry.push(arry[i]);
+}
+
+console.log(newArry);
+
+const newArry2 = arry.map((x) => x * 200);
+const newArry3 = newArry2.filter((y) => y > 4000);
+console.log(newArry2);
+console.log(newArry3);
