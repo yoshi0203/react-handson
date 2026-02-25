eee7a82fe776366fbf56154b97b4bcc74ed5a35b
Yoshito Yamanaka <ymnkswallow@gmail.com>
2026-02-26 00:47:56 +0900

    fix-17_分割代入

--- react-guide-material/03_js_basic/050_destructuring/start/main.js
+++ react-guide-material/03_js_basic/050_destructuring/start/main.js
@@ -1,26 +1,19 @@
-const arry = ["配列1", "配列2", "配列3"];
-console.log(arry[0]);
-console.log(arry[2]);
-
-const obj = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
-console.log(obj.x);
-console.log(obj.y);
 
 const arr = ["Japan", "Tokyo", "Shinjuku"];
-const objAddress = { country: "Japan", state: "Tokyo", city: "Shinjuku" };
+const objAddress = { a: "Japan", b: "Tokyo", c: "Shinjuku" };
 
-const fnArr = (arry) => {
+const fnArr = ([country,state,city]) => {
   console.log("---配列---");
-  console.log(`country: ${arry[0]}`);
-  console.log(`state: ${arry[1]}`);
-  console.log(`city: ${arry[2]}`);
+  console.log(`country: ${country}`);
+  console.log(`state: ${state}`);
+  console.log(`city: ${city}`);
 };
 
-const fnObj = (objAddr) => {
+const fnObj = ({a, b, c }) => {
   console.log("---オブジェクト---");
-  console.log(`country: ${objAddr.country}`);
-  console.log(`state: ${objAddr.state}`);
-  console.log(`city: ${objAddr.city}`);
+  console.log(`country: ${a}`);
+  console.log(`state: ${b}`);
+  console.log(`city: ${c}`);
 };
 
 fnArr(arr);
